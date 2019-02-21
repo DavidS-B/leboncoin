e3031ad8b7557b0bc617f1ca50e30f4421e60146
David S-B <d.sentenac-bossiere@hotmail.fr>
2019-02-21 17:33:05 +0100

    Deploy with push test 4

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -13,7 +13,7 @@ export class SignUp extends Component {
     e.preventDefault();
 
     const response = await axios.post(
-      "mongodb://heroku_pppqmgrd:tsevnakcu159ds2d6j9t1uoje5@ds149855.mlab.com:49855/heroku_pppqmgrd/direct/sign_up",
+      "https://leboncoin-dsb.herokuapp.com/direct/sign_up",
       {
         pseudo: this.state.pseudo,
         email: this.state.email,
